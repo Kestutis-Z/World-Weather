709aeb4f244bf8899eda3d1cf83ed3a5f753b4f9
kestutis_z <haringeymobile@gmail.com>
2016-11-20 11:03:49 +0000

    Shorten app strings according to Android guidelines.  "Fixes #28".

--- WorldWeather/app/src/main/java/com/haringeymobile/ukweather/AboutActivity.java
+++ WorldWeather/app/src/main/java/com/haringeymobile/ukweather/AboutActivity.java
@@ -75,25 +75,48 @@ public class AboutActivity extends ThemedActivity {
     private String getAboutTextPart_2() {
         Resources res = getResources();
 
+        int itemCount = 0;
+
         String aboutText_2 = "\n";
         aboutText_2 += res.getString(R.string.credits_1);
         aboutText_2 += "\n\n";
+        aboutText_2 += getNextItemNumber(itemCount);
+        itemCount++;
         aboutText_2 += res.getString(R.string.credits_2);
         aboutText_2 += res.getString(R.string.credits_3);
         aboutText_2 += res.getString(R.string.credits_4);
         aboutText_2 += res.getString(R.string.credits_5);
+        aboutText_2 += getNextItemNumber(itemCount);
+        itemCount++;
         aboutText_2 += res.getString(R.string.credits_6);
         aboutText_2 += res.getString(R.string.credits_7);
         aboutText_2 += res.getString(R.string.credits_8);
         aboutText_2 += res.getString(R.string.credits_9);
+        aboutText_2 += getNextItemNumber(itemCount);
+        itemCount++;
         aboutText_2 += res.getString(R.string.credits_10);
         aboutText_2 += res.getString(R.string.credits_11);
+        aboutText_2 += res.getString(R.string.credits_115);
         aboutText_2 += res.getString(R.string.credits_12);
         aboutText_2 += res.getString(R.string.credits_13);
+        aboutText_2 += getNextItemNumber(itemCount);
+        itemCount++;
+        aboutText_2 += res.getString(R.string.credits_131);
+        aboutText_2 += res.getString(R.string.credits_132);
+        aboutText_2 += res.getString(R.string.credits_133);
+        aboutText_2 += res.getString(R.string.credits_134);
+        aboutText_2 += res.getString(R.string.credits_135);
+        aboutText_2 += getNextItemNumber(itemCount);
+        itemCount++;
         aboutText_2 += res.getString(R.string.credits_14);
         aboutText_2 += res.getString(R.string.credits_15);
+        aboutText_2 += getNextItemNumber(itemCount);
+        itemCount++;
         aboutText_2 += res.getString(R.string.credits_16);
+        aboutText_2 += res.getString(R.string.credits_165);
         aboutText_2 += res.getString(R.string.credits_17);
+        aboutText_2 += getNextItemNumber(itemCount);
+        itemCount++;
         aboutText_2 += res.getString(R.string.credits_18);
         aboutText_2 += "\n\n";
 
@@ -118,12 +141,17 @@ public class AboutActivity extends ThemedActivity {
         aboutText_2 += "\n";
         aboutText_2 += res.getString(R.string.credits_19);
         aboutText_2 += res.getString(R.string.credits_20);
+        aboutText_2 += getNextItemNumber(itemCount);
         aboutText_2 += res.getString(R.string.credits_21);
         aboutText_2 += res.getString(R.string.credits_22);
         aboutText_2 += res.getString(R.string.credits_23);
         aboutText_2 += res.getString(R.string.credits_24);
 
         return aboutText_2;
+    }
+
+    private String getNextItemNumber(int itemCount) {
+        return "(" + (itemCount + 1) + ") ";
     }
 
     /**
